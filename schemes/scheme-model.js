01d18be51666ac5cc03eeb2e7bb4452ad838385f
Travis Little <kingtrav@gmail.com>
2019-08-27 16:42:31 -0600

    shell of scheme-model.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = '../data/dbConfig.js'
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+
+}
+
+function findById(id) {
+
+}
+
+function findSteps(id) {
+
+}
+
+function add(scheme) {
+
+}
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+    
+}
