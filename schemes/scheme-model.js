aa66fca21a2450e4a43da7824a4a8de25dbdf793
Travis Little <kingtrav@gmail.com>
2019-08-27 18:06:29 -0600

    all helper functions completed

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,4 +1,4 @@
-const db = '../data/dbConfig.js'
+const db = require('../data/dbConfig.js')
 
 module.exports = {
     find,
@@ -10,25 +10,34 @@ module.exports = {
 }
 
 function find() {
-
+    return db('schemes')
 }
 
 function findById(id) {
-
+    return db('schemes').where({ id }).first()
 }
 
 function findSteps(id) {
-
+    return db('steps')
+        .join('schemes', 'steps.scheme_id', 'schemes.id')
+        .select('steps.id', 'steps.step_number', 'schemes.scheme_name')
+        .where({ 'steps.id': id })        
 }
 
 function add(scheme) {
-
+    return db('schemes').insert(scheme)
+        .then(ids => {
+            return findById(ids[0])
+        })
 }
 
 function update(changes, id) {
-
+    return db('schemes').where({ id }).update(changes)
+        .then(count => {
+            return findById(id)
+        })
 }
 
 function remove(id) {
-    
+    return db('schemes').where({ id }).delete()
 }
